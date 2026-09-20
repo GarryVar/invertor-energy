77a7461130bb976faa59aec6a384c4e49fd5c4a7
garryvar <darisvet.sokolov@yandex.ru>
2026-09-20 12:10:15 +0300

    fix/Remove default vite logo for future company logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -53,8 +53,8 @@ function Header() {
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Здесь будет логотип"
+              src=""
               className="h-8 w-auto"
             />
           </a>
