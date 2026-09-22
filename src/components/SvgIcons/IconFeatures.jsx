f9745e1c0ec1c9565321b27bea9a80e3c717eaf2
GarryVar <darisvet.sokolov@yandex.ru>
2026-09-22 10:54:34 +0300

    fix: IconFeaturesToggle icon and size

--- src/components/SvgIcons/IconFeatures.jsx
+++ src/components/SvgIcons/IconFeatures.jsx
@@ -1,6 +1,6 @@
-import IconFeatures from "../../assets/icon/icon_features.svg?react";
+import IconFeatures from "../../assets/icon/icon_arrow.svg?react";
 
 function IconFeaturesToggle() {
-  return <IconFeatures />;
+  return <IconFeatures width="15" height="15" />;
 }
 export default IconFeaturesToggle;
